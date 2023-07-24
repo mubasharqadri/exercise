733d8d1f908c359515c0719733b500fce8fbf5a5
mubasharqadri <33282019+mubasharqadri@users.noreply.github.com>
2023-07-24 16:33:52 +0500

    Add files via upload

---
+++ Examples.ts
@@ -0,0 +1,47 @@
+// Excersie Sr. 2
+let personName: string = 'Hello Mubashir! ';
+console.log(personName+'“would you like to learn some Typescript today?”');
+
+// Excersie Sr. 2
+let displayMessage: string = '"would you like to learn some Typescript today?"';
+console.log(personName + displayMessage);
+
+// Excersie Sr. 3
+personName  = "Mubashir"
+console.log(personName.toUpperCase());
+console.log(personName.toLowerCase());
+
+// Excersie Sr. 4
+let authorName: string = 'Albert Einstein ';
+console.log( authorName+ 'once said, “A person who never made a mistake never tried anything new.”');
+
+// Excersie Sr. 5
+let famouQuote: string = 'once said, “A person who never made a mistake never tried anything new.”';
+console.log( authorName+ famouQuote);
+
+// Excersie Sr. 6
+let famous_Name : string = "  Albert Einstein  "
+console.log('\t'+famous_Name.trim()+'\n');
+
+// Excersie Sr. 7&8
+
+console.log('console.log(5 + 3) = '+(5 + 3));
+console.log('console.log(10 - 2) = '+(10 - 2));
+console.log('console.log(4 * 2) = '+(4 * 2));
+console.log('console.log(16 / 2) = '+(16 / 2));
+
+
+// Excersie Sr. 9
+
+let myFavouriteNumber: number = 7;
+console.log('My Favourite number is '+ myFavouriteNumber); // Comments 
+
+// Excersie Sr. 10 & 11
+
+let myFriends: string[] = ['Imran','Fahad','Jahanzaib']
+console.log('My favourite Friend is '+ myFriends[0]); 
+console.log('My favourite Friend is '+ myFriends[1]);
+console.log('My favourite Friend is '+ myFriends[2]);
+
+
+export{};
